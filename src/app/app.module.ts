80613973f785d854ca2edf9786b515de2af71c3d
Nievas2 <angelgabrielnievas@gmail.com>
2023-11-01 09:21:48 -0300

    estructuración y contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { TechnologiesComponent } from './modules/technologies/technologies.component';
+import { ProyectsComponent } from './modules/proyects/proyects.component';
+import { StudiesComponent } from './modules/studies/studies.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { PerfilComponent } from './modules/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TechnologiesComponent,
+    ProyectsComponent,
+    StudiesComponent,
+    ContactComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
